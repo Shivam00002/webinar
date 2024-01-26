c2b7a60056a7ac62d9e472a6f399f06dc71a5c6b
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-01-27 00:57:28 +0530

    added ui fix

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -8,8 +8,8 @@ export const Events = () => {
       <h2 className="font-bold text-[25px] md:text-[35px] md:mt-10 mt-2 text-center">
         Upcoming Events
       </h2>
-      
-      <div className="flex items-center gap-x-10 overflow-x-scroll">
+
+      <div className="flex items-center gap-x-7 overflow-x-scroll">
         {cards.map((el, index) => {
           return <EventCard key={index} />;
         })}
