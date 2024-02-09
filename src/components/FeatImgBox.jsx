49ffb1a072cb38441f88660c602074da6c222a75
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-02-09 23:05:52 +0530

    Update FeatImgBox.jsx

--- src/components/FeatImgBox.jsx
+++ src/components/FeatImgBox.jsx
@@ -7,6 +7,7 @@ export const FeatImgBox = () => {
         src="https://blogger.googleusercontent.com/img/a/AVvXsEhPyoWvb2_v22ph8Wrzc6gwdINySTiH93RyWRu7ykjcVhbIvdfG_GmHD_n8lluIUuNJi5XOcB-MFRk1a-FP9Ajut0eR4HdbZu-OJz-4fyzsRJVvJ7UsKfphbGUFRcMT3lFCLFe68KmWMW64w__1jeWcE0UfloV0POBSGUQylFpIKgCkPcH5LJ-mSTmz_Ow"
         alt="kickstart"
         className="absolute fill "
+
       />
     </div>
   );
