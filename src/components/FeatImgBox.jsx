b51b3b37b3909eee5c98d78979a72faa6be4926f
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-05-16 23:32:06 +0530

    Update FeatImgBox.jsx

--- src/components/FeatImgBox.jsx
+++ src/components/FeatImgBox.jsx
@@ -4,8 +4,6 @@ export const FeatImgBox = () => {
   return (
     <div className="md:w-[140px]  w-[90px] mx-auto h-[90px] md:h-[140px] relative">
       <img
-
-
  src="https://blogger.googleusercontent.com/img/a/AVvXsEhPyoWvb2_v22ph8Wrzc6gwdINySTiH93RyWRu7ykjcVhbIvdfG_GmHD_n8lluIUuNJi5XOcB-MFRk1a-FP9Ajut0eR4HdbZu-OJz-4fyzsRJVvJ7UsKfphbGUFRcMT3lFCLFe68KmWMW64w__1jeWcE0UfloV0POBSGUQylFpIKgCkPcH5LJ-mSTmz_Ow"
         alt="kickstart"
         className="absolute fill "
