ee1da4f94d4e51945474979fe04cfa1f63e7bd5f
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2025-05-26 23:45:54 +0530

    Update Events.jsx

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -6,6 +6,7 @@ export const Events = () => {
   const [webinars, setWebinars] = useState([]);
 
 
+
   const Api = "https://webinar-backend.vercel.app/data";
 
   const fetchWebinars = async () => {
